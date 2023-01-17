d1e5a790f84bade3de060860b6a260b1300107c4
Vishal Dilwale <vishal.dilwale@mindbowser.com>
2023-01-17 20:11:45 +0530

    browser four commit

--- testCases/conftets.py
+++ testCases/conftets.py
@@ -1,7 +1,28 @@
 from selenium import webdriver
 import pytest
 
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome"
+    )
+
 @pytest.fixture()
-def setup():
-    driver=webdriver.Chrome()
+def setup(request):
+    browser=request.config.getoption('--browser')
+    if browser=='chrome':
+        driver=webdriver.Chrome()
+        print("lanching chrome browser......")
+    elif browser=='firefox':
+        driver=webdriver.Firefox()
+        print("lanching firefox browser......")
+    else:
+        driver=webdriver.Chrome()
     return driver
+
+
+
+
+
+
